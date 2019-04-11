a7d174c58f5690ca21949462d81eceebff053205
AnthonyAmanse <ghieamanse@gmail.com>
2019-04-10 20:44:06 -0700

    use ibmcloud container-registry
    Signed-off-by: AnthonyAmanse <ghieamanse@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+echo -e "Build environment variables:"
+echo "REGISTRY_URL=${REGISTRY_URL}"
+echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
+echo "IMAGE_NAME=${IMAGE_NAME}"
+echo "BUILD_NUMBER=${BUILD_NUMBER}"
 
-# log in docker
-docker login
+# Learn more about the available environment variables at:
+# https://console.bluemix.net/docs/services/ContinuousDelivery/pipeline_deploy_var.html#deliverypipeline_environment
 
-# build test-nodejs
-docker build -t $DOCKER_USERNAME/test-nodejs:$GIT_COMMIT .
+# To review or change build options use:
+# bx cr build --help
 
-# push image
-docker push $DOCKER_USERNAME/test-nodejs:$GIT_COMMIT
+echo -e "Checking for Dockerfile at the repository root"
+if [ -f Dockerfile ]; then
+   echo "Dockerfile found"
+else
+    echo "Dockerfile not found"
+    exit 1
+fi
+
+echo -e "Building container image"
+set -x
+bx cr build -t $REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME:$GIT_COMMIT .
+set +x
